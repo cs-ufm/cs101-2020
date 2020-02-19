43b18ad2a8880ff1939123b117233922b9c98609
@jmarcos-cano <jmarcos.cano@gmail.com>
2020-02-18 19:40:46 -0600

    @jmarcos-cano - pointers

--- c/pointers/array_pointers.c
+++ c/pointers/array_pointers.c
@@ -3,12 +3,14 @@
 int main(){
     int numbers[5];
 
-    int *ptr =  numbers; // or &numbers[0]
+    int* ptr =  numbers; // or &numbers[0]
     int base= 2;
 
 
     // iterate over with pointer
+    // ptr++ will give us the next box
     for (;ptr<&numbers[5]; ptr++ ){
+        // . *ptr is the value it points
         *ptr= base;
         base*=2;
     }
